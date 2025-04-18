992f433ea51f868518cb1a94abf83e2ca2edc51b
javdamonazzam <m131037@gmail.com>
2025-04-18 10:49:48 +0330

    Update page.tsx

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,4 +1,11 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+
 export default function Link() {
+    const searchParams = useSearchParams();
+    console.log(searchParams.get('id') ,"<<<<<<<<<<<<<<<<<<<<<<<<");
+    
+    const id = searchParams.get('id'); 
     return(
         <></>
     )
