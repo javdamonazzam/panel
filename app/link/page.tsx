b011b08771dcd141bb00834d11a15d06758ba1c5
javdamonazzam <m131037@gmail.com>
2025-04-18 10:25:30 +0330

    Update page.tsx

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,45 +1,47 @@
-// 'use client';
+'use client';
 
-// import axios from 'axios';
-// import { useSearchParams } from 'next/navigation';
-// import { useEffect } from 'react';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
-// export default function Link() {
-//   const searchParams = useSearchParams();
-//   const id = searchParams.get('id'); // اگه آدرس مثل ?id=4564 باشه
+export default function Link() {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id'); // اگه آدرس مثل ?id=4564 باشه
 
-//   useEffect(() => {
-//     if (!id) return;
+  // useEffect(() => {
+  //   if (!id) return;
 
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('http://79.133.46.247:3000/list');
-//         const cleanedData = response.data
-//           .split('\n')
-//           .filter((line: string) => line.trim() !== '')
-//           .map((line: string) => line.replace(/^\s*\d+\)\s*/, ''));
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('http://79.133.46.247:3000/list');
+  //       const cleanedData = response.data
+  //         .split('\n')
+  //         .filter((line: string) => line.trim() !== '')
+  //         .map((line: string) => line.replace(/^\s*\d+\)\s*/, ''));
 
-//         if (cleanedData.includes(id)) {
-//           const create = await axios.get(`http://79.133.46.247:3000/create?publicKey=${id}`);
-//           const config = create.data.replace('79.133.46.247', 'be.jettingwire.xyz');
-//           const blob = new Blob([config], { type: 'text/plain;charset=utf-8' });
+  //       if (cleanedData.includes(id)) {
+  //         const create = await axios.get(`http://79.133.46.247:3000/create?publicKey=${id}`);
+  //         const config = create.data.replace('79.133.46.247', 'be.jettingwire.xyz');
+  //         const blob = new Blob([config], { type: 'text/plain;charset=utf-8' });
 
-//           const link = document.createElement('a');
-//           link.href = URL.createObjectURL(blob);
-//           link.download = `${id}.ovpn`;
-//           document.body.appendChild(link);
-//           link.click();
-//           document.body.removeChild(link);
-//         } else {
-//           alert('❌ لینک کن منقضی شد');
-//         }
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
+  //         const link = document.createElement('a');
+  //         link.href = URL.createObjectURL(blob);
+  //         link.download = `${id}.ovpn`;
+  //         document.body.appendChild(link);
+  //         link.click();
+  //         document.body.removeChild(link);
+  //       } else {
+  //         alert('❌ لینک کن منقضی شد');
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-//     fetchData();
-//   }, [id]);
+  //   fetchData();
+  // }, [id]);
 
-//   return <></>;
-// }
+  return (
+    <></>
+  );
+}
